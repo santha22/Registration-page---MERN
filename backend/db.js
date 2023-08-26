73ba4fc56ed94ec3dbd946d6874d22700f356824
Savara Santha Kumar <savarasanthakumar20@gmail.com>
2023-08-26 19:29:22 +0530

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    };
+    try {
+        mongoose.connect("mongodb://127.0.0.1:27017",  connectionParams);
+        console.log("Connected to database successfully");
+    } catch (error) {
+        console.log(error);
+        console.log("could not connect to database");
+
+    }
+}
